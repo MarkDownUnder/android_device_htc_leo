4cd62eb919cff1c771055b2b2c3260dbbaa5e51f
MarkDownUnder <maxim.schwalm@googlemail.com>
2014-11-21 01:08:58 +0100

    leo: Update for AOSP

--- aosp_leo.mk
+++ aosp_leo.mk
@@ -2,17 +2,20 @@
 TARGET_USE_KEYBOARD := international
 
 # Call this first so apn list is actually copied
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product-if-exists, vendor/cm/config/gsm.mk)
 
 # Inherit AOSP device configuration for leo.
 $(call inherit-product, device/htc/leo/device_leo.mk)
 
 # Inherit some common evervolv stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product-if-exists, vendor/cm/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_MODEL := HTC HD2
-PRODUCT_MANUFACTURER := HTC
-PRODUCT_BRAND := htc
+PRODUCT_NAME := aosp_leo
 PRODUCT_DEVICE := leo
-PRODUCT_NAME := cm_leo
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on HD2
+PRODUCT_MANUFACTURER := HTC
+
+
+
